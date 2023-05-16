b1c06627acdcd1456b5fa4b55cd6da823e2826d3
unknown <13570526458@163.com>
2023-05-16 19:19:46 +0800

    init

---
+++ app/src/guangzhouplayer/java/com/example/player/BuildConfig.java
@@ -0,0 +1,11 @@
+package com.example.player;
+
+
+public class BuildConfig {
+    public static final String APP_NAME = "广州播放器";
+    public static final String APPLICATION_ID = "com.example.guangzhouplayer";
+    public static final String BUILD_TYPE = "debug";
+    public static final int VERSION_CODE = 1;
+    public static final String VERSION_NAME = "1.0";
+    public static final String BaseUrl = "https://guangzhouplayer";
+}

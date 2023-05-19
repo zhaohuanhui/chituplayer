a5c2905b9dad9320c6f7229439b57f4ac5626b70
zhh <13570524658@163.com>
2023-05-19 19:54:49 +0800

    [update]

--- app/src/main/java/com/example/player/main/navigation/home/HomeFragment.java
+++ app/src/main/java/com/example/player/main/navigation/home/HomeFragment.java
@@ -1,10 +1,13 @@
 package com.example.player.main.navigation.home;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
@@ -54,6 +57,12 @@ public class HomeFragment extends Fragment {
         initFragment();
         initViewPage();
         onClick();
+        if (Build.VERSION.SDK_INT >= 21){
+            View decorView = getActivity().getWindow().getDecorView();
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            getActivity().getWindow().setStatusBarColor(Color.TRANSPARENT);
+
+        }
         return inflate;
     }
 
